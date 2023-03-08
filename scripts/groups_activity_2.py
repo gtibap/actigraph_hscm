dd21812a935d1a236700c6ec426832770ad7c30d
gtibap <gtibap@gmail.com>
2023-03-08 13:49:07 -0500

    Add files via upload

--- scripts/groups_activity_2.py
+++ scripts/groups_activity_2.py
@@ -54,15 +54,15 @@ def non_motion_periods(mag_col):
     return duration_active, duration_inactive, start_active
 
 
-def activity_sectors(df, column):
+def activity_sectors(df, vector_mag, min_value, min_gap):
     
     # nights_list = df['night'].unique().tolist()
-    min_value = 3 # min intensity value
+    # min_value = 3 # min intensity value
     # for night_num in nights_list[:1]:
     # mag_col = df.loc[(df['night']==night_num), ['Vector Magnitude']]
-    min_gap = 10 # seconds
+    # min_gap = 10 # seconds
 
-    df['activity'] = df[column] > min_value
+    df['activity'] = df[vector_mag] > min_value
     # boolean array where True means activity higher than min_value
     activity_arrray = df['activity'].to_numpy()
     # print('active_arrray: ', active_arrray)
@@ -150,24 +150,49 @@ def activity_sectors(df, column):
     print('len(new_activity_array): ', len(new_activity_array))
     print('len(activity_array): ', len(activity_arrray))
 
-    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True)
-    axes[0].plot(df[column].to_numpy())
-    axes[1].plot(activity_arrray)
-    axes[2].plot(new_activity_array)
-    plt.show()
+    # fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True)
+    # axes[0].plot(df[vector_mag].to_numpy())
+    # # axes[1].plot(activity_arrray)
+    # axes[1].plot(new_activity_array)
+    # axes[2].plot(df[incl_off].to_numpy())
 
+    # plt.show()
 
+    return new_activity_array
 
-    
+def activity_intervals(activity_vector):
 
-    return
+    changes_activity = activity_vector[:-1] != activity_vector[1:]
+    # changes_array is a boolean vector; True means a change; False means no change
+    # indices or location of Trues (changes) values; +1 because I want the index when the data already changed from left to right
+    idx_changes = np.flatnonzero(changes_activity) + 1
+
+    # at least three detected changes
+    if len(idx_changes) > 3:
+        if activity_vector[0]==False:
+            idx_ini=idx_changes[0::2]
+            idx_end=idx_changes[1::2]
+        else:
+            idx_ini=idx_changes[1::2]
+            idx_end=idx_changes[2::2]
+
+        # removing the initial index of the last sector if it has not finished
+        if activity_vector[-1]==True:
+            idx_ini=idx_ini[0:-1]
+        else:
+            pass
+    else:
+        idx_ini=[0]
+        idx_end=[0]
+
+    return idx_ini, idx_end
 
 ####### main function ###########
 if __name__== '__main__':
 
     # Get the list of all files and directories
     path = "../data/all_data_1s/"
-    path_out = "../data/results_each_pose/"
+    path_out = "../data/results_motion/"
     files_list = os.listdir(path)
     
     print("Files in '", path, "' :")
@@ -197,160 +222,33 @@ if __name__== '__main__':
             # plot 'Vector Magnitude' night by night
             nights_list = df_nights['night'].unique().tolist()
             # print('nights: ', nights_list)
-            for night_num in nights_list[:2]:
+            df_active_nights = pd.DataFrame(columns=['night','t_ini','t_end'])
+            for night_num in nights_list:
                 # print('night: ', night_num)
                 df_n = df_nights.loc[(df_nights['night']==night_num), [vec_mag]]
                 # print('df_n.info: ', df_n.info())
                 # print(df_n.head())
                 # print(df_n.tail())
 
-                # identifying sectors of activity per night
-                activity_sectors(df_n, vec_mag)
+                # identifying sectors of activity per night where gaps of 10s of inactivity are considered part of the activity section
+                min_gap=10 # seconds
+                min_value=3 # Vector Magnitude should be greater than this value to be considered as a valid motor activity
+                activity_vector=activity_sectors(df_n, vec_mag, min_value, min_gap)
+                idx_ini, idx_end = activity_intervals(activity_vector)
+                print('idx_ini: ', len(idx_ini), idx_ini)
+                print('idx_end: ', len(idx_end), idx_end)
+                df_act_night = pd.DataFrame(columns=['night','t_ini','t_end'])
+                df_act_night['t_ini']=idx_ini
+                df_act_night['t_end']=idx_end
+                df_act_night['night']=night_num
+                df_active_nights=pd.concat([df_active_nights, df_act_night], ignore_index=True)
+
+            # print(df_active_nights)
+            # save on disk df_active_nights
+            df_active_nights.to_csv(path_out+'active_'+sample, index=False)
             
         except ValueError:
             print('Problem reading the file', sample, '... it is skipped.')
 
     
-
-    # # the header in line 10 of the csv file
-    # header_location=10
-    # df1 = pd.read_csv("../data/p00/Turner Chest1secDataTable.csv", header=header_location, decimal=',', usecols=['Date',' Time',' Axis1','Axis2','Axis3','Vector Magnitude'])
-    # # print(df.empty)
-    # # print(df.shape)
-    # print(df1.columns)
-    # print(df1.head)
-    # print(df1.info())
-    
-    # df2 = pd.read_csv("../data/p00/Turner Thigh1secDataTable.csv", header=header_location, decimal=',', usecols=['Date',' Time',' Axis1','Axis2','Axis3','Vector Magnitude'])
-    # # print(df.empty)
-    # # print(df.shape)
-    # print(df2.columns)
-    # # print(df.head)1
-    # print(df2.info())
-    
-    
-    # print(df.describe())
-    # print(df['Inclinometer Off'].unique())
-    # print(df['Inclinometer Off'].value_counts())
-    # print(df.loc[(df['Date']=='13/12/2022'), ['Date',' Time','Vector Magnitude']])
-    # print(df.loc[(df['Date']=='13/12/2022') & (df['Vector Magnitude'].isnull()), ['Date',' Time','Vector Magnitude']])
-    # print(df.loc[(df['Date']=='13/12/2022') & ((df[' Axis1']>0)|(df['Axis2']>0)|(df['Axis3']>0)), ['Date',' Time',' Axis1','Axis2','Axis3','Vector Magnitude']])
-
-    # print(df.loc[(df['Date']=='12/12/2022') & (df[' Time']>='22:00:00'), ['Date',' Time',' Axis1','Axis2','Axis3','Vector Magnitude']])
-    # print(df.loc[(df['Date']=='13/12/2022') & (df[' Time']<='10:00:00'), ['Date',' Time',' Axis1','Axis2','Axis3','Vector Magnitude']])
-
-    # plot_data(df1, 'chest')
-    # plot_data(df2, 'thigh')
-    # plt.show()
-
-    # time_start='22:00:00'
-    # time_end='10:00:00'
-    # same_day=False
-
-    # time_start='04:45:00'
-    # time_end='05:45:00'
-    # same_day=True
-
-
-    # df_chest = getSelectedData(df1, time_start, time_end, same_day)
-    # df_thigh = getSelectedData(df2, time_start, time_end, same_day)
-    # print(df1_nights.columns)
-    # print(df1_nights.info())
-    # print(df1_nights.head)
-
-    # plot_nights(df_chest, df_thigh)
-    # plt.show()
-
-
-    # histograms
-
-    # plt.figure(1)
-    # nights_list = df_chest['night'].unique().tolist()
-    # for night_num in nights_list[:1]:
-    #     mag_col = df_chest.loc[(df_chest['night']==night_num), ['Vector Magnitude']]
-    #     activity, inactivity = non_motion_periods(mag_col)
-    #     plt.subplot(211)
-    #     activityHistogram(activity, inactivity)
-
-    # nights_list = df_thigh['night'].unique().tolist()
-    # for night_num in nights_list[:1]:
-    #     mag_col = df_thigh.loc[(df_thigh['night']==night_num), ['Vector Magnitude']]
-    #     activity, inactivity = non_motion_periods(mag_col)
-    #     plt.subplot(212)
-    #     activityHistogram(activity, inactivity)
-
-    # plt.show()
-    
-    #
-    # empty dataframe initialization
-    # df_all = pd.DataFrame(columns=names_columns)
-
-    max_list_chest=[]
-    max_list_thigh=[]
-    df_freqs = pd.DataFrame(columns=['night', 'location', 'activity','inactivity'])
-
-    # extracting blocs of data per night
-    nights_list = df_chest['night'].unique().tolist()
-    all_count_groups=[]
-    for night_num in nights_list:
-
-        df_mag_chest = df_chest.loc[(df_chest['night']==night_num), ['Vector Magnitude']]
-        df_mag_thigh = df_thigh.loc[(df_chest['night']==night_num), ['Vector Magnitude']]
-
-        # chest        
-        # duration of continuous periods of actitity and inactivity in seconds
-        activity, inactivity, start_active = non_motion_periods(df_mag_chest)
-
-        # print ('results', len(activity), len(inactivity), start_active)
-        # print('activity: ', activity)
-        # print('inactivity: ', inactivity)
-
-        min_gap = 10 # seconds
-        small_gap =  inactivity < min_gap
-        # print('small_gap: ', small_gap)
-    
-        long_result = np.array([])
-        new_activity = list()
-        count_groups=0
-        id_ac = 0
-        if start_active == True:
-            id_in = 0
-        else:
-            id_in = 1
-            # number of zeros; one zero per second
-            long_result = np.concatenate((long_result, np.zeros(inactivity[0], np.int8)), axis=None)
-        
-        # print('id_in: ', id_in)
-        while id_ac < (len(activity)-1):
-            acc_temp = activity[id_ac]
-            acc_neta = activity[id_ac]
-            # true means that the gap is less than min_gap
-            while (id_in < len(small_gap)) and (small_gap[id_in]) and (id_ac < (len(activity)-1)):
-                id_ac+=1
-                acc_temp += activity[id_ac]
-                acc_neta += activity[id_ac] + inactivity[id_in]
-                id_in+=1
-            new_activity.append(acc_temp)
-            # 1 per activity group per second
-            # longitudinal_result.append(acc_neta*[1])
-            long_result = np.concatenate((long_result, np.ones(acc_neta,np.int8)), axis=None)
-            # 0 per inactivity group per second
-            # longitudinal_result.append(inactivity[id_in]*[0])
-            long_result = np.concatenate((long_result, np.zeros(inactivity[id_in],np.int8)), axis=None)
-            count_groups+=1
-            id_ac+=1
-            id_in+=1
-        
-        print('count_groups: ', count_groups)
-        all_count_groups.append(count_groups)
-
-
-
-    nights = list(range(0,len(all_count_groups)))
-    print(nights)
-    print(all_count_groups)
-    plt.bar(nights[:-1], all_count_groups[:-1])
-    plt.xlabel('night')
-    plt.ylabel('count of detected motions')
-    plt.show()
 
