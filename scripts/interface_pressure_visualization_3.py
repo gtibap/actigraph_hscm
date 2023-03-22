480473c532878d3e651aa6774ea673b37da07fb8
gtibap <gtibap@gmail.com>
2023-03-22 18:26:33 -0400

    Add files via upload

--- scripts/interface_pressure_visualization_3.py
+++ scripts/interface_pressure_visualization_3.py
@@ -4,32 +4,35 @@ import csv
 import cv2
 import matplotlib.pyplot as plt
 import datetime
+import sys
 
 ##########
 # global variables
-figure, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(8, 6))
+figure_1, ax_1 = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(8, 6))
+figure_2, ax_2 = plt.subplots(figsize=(6, 8))
 drawing = False
 x0=0
 y0=0
 x1=0
 y1=0
+pressed_key='nan'
 # global variables
 ##########
 
 
-def resize_img(img):
-    # print('Original Dimensions : ',img.shape)
+# def resize_img(img):
+#     # print('Original Dimensions : ',img.shape)
  
-    scale = 10 # percent of original size
-    width = int(img.shape[1] * scale)
-    height = int(img.shape[0] * scale)
-    dim = (width, height)
+#     scale = 10 # percent of original size
+#     width = int(img.shape[1] * scale)
+#     height = int(img.shape[0] * scale)
+#     dim = (width, height)
     
-    # resize image
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+#     # resize image
+#     resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     
-    # print('Resized Dimensions : ',resized.shape)
-    return resized
+#     # print('Resized Dimensions : ',resized.shape)
+#     return resized
 
 
 def average_data(df_head, raw_data):
@@ -94,51 +97,65 @@ def average_data(df_head, raw_data):
     return df_ts, data_mean
 
 
-def plot_actigraphy(chest,thigh,time):
-    global figure, ax
-    ax[0].cla()
-    ax[1].cla()
-    ax[0].plot(thigh)
-    ax[1].plot(chest)
+def plot_pressure(img):
+    global figure_2, ax_2
 
-    ax[0].legend(['thigh'])
-    ax[1].legend(['chest'])
+    ax_2.cla()
+    ax_2.imshow(img)
+
+    figure_2.canvas.draw()
+    figure_2.canvas.flush_events()
+
+    return
+
+def plot_actigraphy(img,chest,thigh,time):
+    global figure_1, figure_2, ax_1, ax_2
+    ax_1[0].cla()
+    ax_1[1].cla()
+    ax_2.cla()
+
+    ax_1[0].plot(thigh)
+    ax_1[1].plot(chest)
 
     # only one line may be specified; full height
-    ax[0].axvline(x = time, color = 'r', label = 'axvline - full height')
-    ax[1].axvline(x = time, color = 'r', label = 'axvline - full height')
+    ax_1[0].axvline(x = time, color = 'r', label = 'axvline - full height')
+    ax_1[1].axvline(x = time, color = 'r', label = 'axvline - full height')
 
-    figure.canvas.draw()
-    figure.canvas.flush_events()
+    ax_2.imshow(img)
+
+    ax_1[0].legend(['thigh'])
+    ax_1[1].legend(['chest'])
+
+    ax_2.set_xlabel('pixel')
+    ax_2.set_ylabel('pixel')
+
+    figure_1.canvas.draw()
+    figure_2.canvas.draw()
+
+    figure_1.canvas.flush_events()
+    figure_2.canvas.flush_events()
 
     return
 
 
-#define the events for the
-# mouse_click.
-def mouse_interact(event, x, y, flags, param):
-    global drawing, x0,y0,x1,y1
-      
-    # to check if left mouse 
-    # button was clicked
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        x0,y0=x,y
-        # print('x0,y0:', x0,y0)
+def onclick(event):
+    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f, name=%s' %
+          ('double' if event.dblclick else 'single', event.button,
+           event.x, event.y, event.xdata, event.ydata, event.name))
+   
     
-    elif event == cv2.EVENT_MOUSEMOVE and drawing == True:
-        x1,y1=x,y
-        print('x0,y0 x1,y1:', x0,y0, x1,y1)
-    
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-    
-    else:
-        pass
-    
-    return
 
+def on_press(event):
+    global pressed_key
 
+    print('press', event.key)
+    pressed_key = event.key
+    sys.stdout.flush()
+
+    # if event.key == 'x':
+    #     visible = xl.get_visible()
+    #     xl.set_visible(not visible)
+    #     fig.canvas.draw()
 
 ####### main function ###########
 if __name__== '__main__':
@@ -190,15 +207,23 @@ if __name__== '__main__':
     df_chest_a = df_chest.loc[(df_chest[' Time']>=time_ini) & (df_chest[' Time']<=time_end)]
     df_thigh_a = df_thigh.loc[(df_thigh[' Time']>=time_ini) & (df_thigh[' Time']<=time_end)]
     
-    # activate interactive mouse actions on window
-    cv2.namedWindow('image')
-    cv2.setMouseCallback('image',mouse_interact)
+    # # activate interactive mouse actions on window
+    # cv2.namedWindow('image')
+    # cv2.setMouseCallback('image',mouse_interact)
 
     # to run GUI event loop
+    cid1 = figure_1.canvas.mpl_connect('button_press_event', onclick)
+    cid2 = figure_2.canvas.mpl_connect('button_press_event', onclick)
+    figure_1.canvas.mpl_connect('key_press_event', on_press)
+    figure_2.canvas.mpl_connect('key_press_event', on_press)
     plt.ion()
     plt.show()
+    print('cid1: ', cid1)
+    print('cid2: ', cid2)
+    
 
     id_frame=0
+    step=0
     exit_key=False
     flag =True
     flag_start=True
@@ -207,33 +232,32 @@ if __name__== '__main__':
     while (exit_key==False) and (id_frame <len(frames_sec)) and flag:
 
         frame=frames_sec[id_frame]
+       
+        plot_actigraphy(frame, df_chest_a[vec_mag].to_numpy(), df_thigh_a[vec_mag].to_numpy(), id_frame)
+        # plt.pause(.01)
 
-        s_f = frame*2.5
-        s_f[s_f>255.0]=255.0
-        s_f = s_f.astype(np.uint8)
-        s_f=resize_img(s_f)
-        
-        colormap = plt.get_cmap('plasma')
-        heatmap = (colormap(s_f) * 2**16).astype(np.uint16)[:,:,:3]
-        heatmap = cv2.cvtColor(heatmap, cv2.COLOR_RGB2BGR)
+        # if flag_start == True:
+        #     pressed_key = cv2.waitKey(0) # miliseconds
+        #     flag_start=False
+        # else:
+        #     pressed_key = cv2.waitKey(100) # miliseconds
 
-        cv2.imshow('image',heatmap)
-
-        # plot_actigraphy(df_chest_a.iloc[0:id_frame][vec_mag].to_numpy(), df_thigh_a.iloc[0:id_frame][vec_mag].to_numpy())
-        plot_actigraphy(df_chest_a[vec_mag].to_numpy(), df_thigh_a[vec_mag].to_numpy(), id_frame)
-
-        if flag_start == True:
-            pressed_key = cv2.waitKey(0) # miliseconds
-            flag_start=False
-        else:
-            pressed_key = cv2.waitKey(100) # miliseconds
-
-        if pressed_key == ord('a'):
-            print ("pressed a")
+        if pressed_key == 'x':
+            print ("pressed x")
             exit_key=True
-        else:
-            print('id frame: ', id_frame, df_f.iloc[id_frame]['time_stamp'])
-            id_frame+=1
+        elif pressed_key == 'h':
+            step=1
+        elif pressed_key == 'd':
+            step=0
+
+        # else:
+        #     print('id frame: ', id_frame, df_f.iloc[id_frame]['time_stamp'])
+        #     id_frame+=1
+
+        # print('id frame: ', id_frame, df_f.iloc[id_frame]['time_stamp'])
+        id_frame+=step
     
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    plt.close('all')
+    # plt.show()
